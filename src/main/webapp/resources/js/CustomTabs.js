5b19a215a9a99b4f4907f483d2f2c97545a3632d
Lorderot <Ihorkayf_@mail.ru>
2017-05-13 22:02:54 +0300

    Booking jsp logic

---
+++ src/main/webapp/resources/js/CustomTabs.js
@@ -0,0 +1,13 @@
+var selectedRoomType;
+function selectRoomType(evt, roomType) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    evt.currentTarget.className += " active";
+}
