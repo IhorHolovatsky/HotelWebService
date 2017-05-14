535e9c1632148e5d899ed209811b5dee80859806
Lorderot <Ihorkayf_@mail.ru>
2017-05-14 17:13:38 +0300

    pizdec, kak tak

--- src/main/java/com/hws/viewModels/BookingViewModel.java
+++ src/main/java/com/hws/viewModels/BookingViewModel.java
@@ -1,14 +1,19 @@
 package com.hws.viewModels;
 
 import com.hws.hibernate.models.Room;
+import lombok.Data;
 
 import java.util.Date;
 
 /**
  * Created by olecs on 5/13/2017.
  */
+@Data
 public class BookingViewModel {
     public Room Room;
-    public Date StartDate;
-    public Date EndDate;
+    public String StartDate;
+    public String EndDate;
+
+    public String RoomId;
+    public String UserId;
 }
